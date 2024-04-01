c78807ea3b703ca693c5e8bbfa0c90b252bdade1
D0KKKA <143734856+D0KKKA@users.noreply.github.com>
2024-04-01 13:07:14 +0500

    profile

--- users/serializers.py
+++ users/serializers.py
@@ -27,6 +27,15 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             'password',
         ]
         extra_kwargs = {'password': {'write_only': True}}
+    def create(self, validated_data):
+        user = models.User(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class LoginSerializer(serializers.Serializer):
@@ -57,14 +66,6 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = models.User
         fields = ['first_name', 'last_name', 'email', 'image',  'date_of_birth']
 
-    def create(self, validated_data):
-        user = models.User(
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+
 
 
