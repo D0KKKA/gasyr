b530df015c93486e1d3cb02375f884a05f3454b6
D0KKKA <143734856+D0KKKA@users.noreply.github.com>
2024-05-04 21:40:19 +0500

    fix

--- gasyrlab/settings.py
+++ gasyrlab/settings.py
@@ -209,11 +209,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
 
-]
 
 MEDIA_URL = '/Media/'
 MEDIA_ROOT = BASE_DIR/'Media'
